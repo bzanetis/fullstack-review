ba495dcd9034522fedfb546f9c8e0f78fa084c6f
bzanetis <britt.zanetis19@gmail.com>
2019-02-01 15:16:46 -0600

    init commit, setup and schema

--- database/index.js
+++ database/index.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  id: Number,
+  name: String,
+  owner: { login: { type: String }},
+  forks: Number,
+  watchers: Number
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
